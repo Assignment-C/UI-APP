13ee64e928e4d6ff3f1ccdbdd004b3b02c79354b
yem hea <heayem7@gmail.com>
2021-10-31 22:51:11 +0700

    Hello

--- Group4/hea.cpp
+++ Group4/hea.cpp
@@ -6,6 +6,20 @@ hea::hea(QWidget *parent) :
     ui(new Ui::hea)
 {
     ui->setupUi(this);
+    ui->comboBox->addItem("Simple Test");
+    ui->comboBox->addItem("Easy");
+    ui->comboBox->addItem("Normal");
+    ui->comboBox->addItem("Hard");
+    ui->comboBox_2->addItem("Academic Test");
+    ui->comboBox_2->addItem("ILTS");
+    ui->comboBox_2->addItem("TOELF");
+    ui->comboBox_3->addItem("Advance Test");
+    ui->comboBox_3->addItem("ITC");
+    ui->comboBox_3->addItem("IFL");
+    ui->comboBox_3->addItem("KIT");
+    ui->comboBox_3->addItem("NEPTIC");
+
+
 }
 
 hea::~hea()
